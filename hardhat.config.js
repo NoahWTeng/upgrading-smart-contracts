241869bb362adb9b974ed0e1aafb21426867872b
noah <noah.8station@gmail.com>
2022-03-07 19:02:35 +0100

    advances

---
+++ hardhat.config.js
@@ -0,0 +1,65 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const ETHERSCAN_API_KEY =
+  process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
+
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL || "https://rinkeby.infura.io/v3/PROJECT-ID";
+
+const MNEMONIC = process.env.MNEMONIC || ".....secret keys";
+// optional
+
+console.log(ETHERSCAN_API_KEY);
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    local: {
+      url: "http://127.0.0.1:8545/",
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      // accounts: [PRIVATE_KEY],
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+      saveDeployments: true,
+    },
+  },
+  solidity: "0.8.4",
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    feeCollector: {
+      default: 1,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
